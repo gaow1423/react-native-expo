0f4feeb6d712fc7e26010ec937a4edb3a4163f15
Wei Gao <gaow1423@gmail.com>
2020-04-16 23:35:04 -0700

    d
    d

---
+++ rn-guess-number/screens/GameOverScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game is Over!</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userName}</Text>
+      <Button title="New Game" onPress={props.onRestart}></Button>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default GameOverScreen;
